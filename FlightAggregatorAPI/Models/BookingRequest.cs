6a5c74e41cf9b1f24af9506eea73a5500c4d4e64
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-07-25 10:04:32 +0500

    Created Models

---
+++ FlightAggregatorAPI/Models/BookingRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightAggregatorAPI.Models
+{
+    public class BookingRequest
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FlightNumber { get; set; }
+        public string PassengerName { get; set; }
+        public string PassengerEmail { get; set; }
+    }
+}
---
+++ FlightAggregatorAPI/Models/FlightInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightAggregatorAPI.Models
+{
+    public class FlightInfo
+    {
+        [Key]
+        public string FlightNumber { get; set; }
+        public string Airline { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public decimal Price { get; set; }
+        public int Stops { get; set; }
+    }
+}
