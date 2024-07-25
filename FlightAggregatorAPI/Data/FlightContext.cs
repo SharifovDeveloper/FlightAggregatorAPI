e55f42e245f56f9d449d221229ac6a746e8d9b18
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-07-25 10:06:20 +0500

    Created FlightContext

---
+++ FlightAggregatorAPI/Data/FlightContext.cs
@@ -0,0 +1,13 @@
+﻿using FlightAggregatorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightAggregatorAPI.Data
+{
+    public class FlightContext : DbContext
+    {
+        public FlightContext(DbContextOptions<FlightContext> options) : base(options) { }
+
+        public DbSet<FlightInfo> Flights { get; set; }
+        public DbSet<BookingRequest> Bookings { get; set; }
+    }
+}
